f78732564f6060ecd01b75db6630161e2e7abae5
R1 <emartin@cesi.fr>
2023-05-02 14:27:36 +0000

    bug correction dynamixel arm control

--- scripts/moveit_controller_bridge.py
+++ scripts/moveit_controller_bridge.py
@@ -10,10 +10,7 @@ class MoveitBridge():
         rospy.init_node('moveit_controller_bridge')
         rospy.wait_for_service('move_planning_service')
         self.serviceClient = rospy.ServiceProxy( 'move_planning_service', MoveitController)
-        rospy.Subscriber(
-            'display_planned_path',
-            DisplayTrajectory,
-            self.display_callback)
+        rospy.Subscriber('/move_group/display_planned_path', DisplayTrajectory, self.display_callback)
     
     def run(self):
         rospy.spin()
@@ -21,40 +18,49 @@ class MoveitBridge():
 
 
     def display_callback(self, display_msg):
-        # print(display_msg.trajectory[0].joint_trajectory.points)
-        motorIDs = array('B',[])
-        for name in display_msg.trajectory[0].joint_trajectory.joint_names:
-            #print(name[-1])
-            motorIDs.append(int(name[-1]))
-        #print(display_msg.trajectory[0].joint_trajectory.points)
-        totalTime = display_msg.trajectory[0].joint_trajectory.points[-1].time_from_start.sec + display_msg.trajectory[0].joint_trajectory.points[-1].time_from_start.nanosec * 0.000000001
-        nbSteps = len(display_msg.trajectory[0].joint_trajectory.points)
-        for i in range (len(display_msg.trajectory[0].joint_trajectory.points)):
-            step = display_msg.trajectory[0].joint_trajectory.points[i]
-            print(step)
-            print("positions")
-            print(step.positions)
-            # print("velocities")
-            # print(step.velocities)
-            # print("accelerations")
-            # print(step.accelerations)
-            # print("-------------------")      
-             # Wait for the service to become available
-                       
-            self.move(motorIDs, array('f', step.positions), array('f', step.velocities), array('f', step.accelerations))
-            print(totalTime)
-            time.sleep(totalTime/nbSteps) #time to wait between two steps
+        try:
+            print("aaaaaaaah")
+            # print(display_msg.trajectory[0].joint_trajectory.points)
+            motorIDs = []
+            for name in display_msg.trajectory[0].joint_trajectory.joint_names:
+                #print(name[-1])
+                motorIDs.append(int(name[-1]))
+            #print(display_msg.trajectory[0].joint_trajectory.points)
+            totalTime = display_msg.trajectory[0].joint_trajectory.points[-1].time_from_start.secs + display_msg.trajectory[0].joint_trajectory.points[-1].time_from_start.nsecs * 0.000000001
+            nbSteps = len(display_msg.trajectory[0].joint_trajectory.points)
+            for i in range (len(display_msg.trajectory[0].joint_trajectory.points)):
+                step = display_msg.trajectory[0].joint_trajectory.points[i]
+                print(step)
+                print("positions")
+                print(step.positions)
+                # print("velocities")
+                # print(step.velocities)
+                # print("accelerations")
+                # print(step.accelerations)
+                # print("-------------------")      
+                # Wait for the service to become available
+                        
+                self.move(motorIDs, array('f', step.positions), array('f', step.velocities), array('f', step.accelerations))
+                print(totalTime)
+                time.sleep(totalTime/nbSteps) #time to wait between two steps
+        except Exception as e:
+            print(e)
 
     def move(self, motorIds, GoalPoses, Speeds, Accelerations):
-        request = MoveitController.Request()
-        request.motor_ids = motorIds
-        request.goal_poses = GoalPoses
-        request.speeds = Speeds
-        request.accelerations = Accelerations 
+        # request = MoveitController()
+        # request.motor_ids = motorIds
+        # request.goal_poses = GoalPoses
+        # request.speeds = Speeds
+        # request.accelerations = Accelerations 
        
-
+        print("-----------------------------")
+        print(motorIds)
+        print(GoalPoses)
+        print(Speeds)
+        print(Accelerations)
+        print("-----------------------------")
         # Call the service
-        future = self.serviceClient(request)
+        future = self.serviceClient(motorIds, GoalPoses, Speeds, Accelerations)
 
         # # Wait for the service call to complete
         # rclpy.spin_until_future_complete(self, future)
@@ -62,9 +68,11 @@ class MoveitBridge():
         #     self.get_logger().info('Result of service call: %d' % future.result().success)
     
 def main(args=None):
-
-    node = MoveitBridge()
-    node.run()
+    try:
+        node = MoveitBridge()
+        node.run()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
